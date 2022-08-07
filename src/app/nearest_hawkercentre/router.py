41848d97cc3c6557d9776e0b6847ef8f3146f564
vinniechu <vinniechuyixuan@gmail.com>
2022-08-08 01:35:40 +0800

    add endpoint router

---
+++ src/app/nearest_hawkercentre/router.py
@@ -0,0 +1,25 @@
+from fastapi import APIRouter
+
+from app.core.logger import logger
+
+from app.nearest_hawkercentre.models import InputLatLon
+from app.nearest_hawkercentre.models import NearestHawkerCentre
+from app.nearest_hawkercentre.models import NearestHawkerCentreEndpointLog
+from app.nearest_hawkercentre.utils import get_nearest_hawkercentre
+
+router = APIRouter()
+
+@router.post("/nearest_hawkercentre", response_model=NearestHawkerCentre)
+async def nearest_hawkercentre(
+    param: InputLatLon
+):
+
+    response = get_nearest_hawkercentre(param)
+
+    logger.info(
+        NearestHawkerCentreEndpointLog(
+            param=param,
+            response=response
+        ).json()
+    )
+    return response
