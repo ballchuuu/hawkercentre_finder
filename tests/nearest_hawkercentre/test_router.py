6f28c6623d7bf2b4602a641165ddc3db6612f5a5
vinniechu <vinniechuyixuan@gmail.com>
2022-08-09 19:53:29 +0800

    add endpoint test cases

---
+++ tests/nearest_hawkercentre/test_router.py
@@ -0,0 +1,36 @@
+from app.nearest_hawkercentre.models import InputLatLon
+
+router_prefix = "/finder"
+
+class TestNearestHawkerCentre:
+    def test_successful_endpoint(self, auth_client):
+        response = auth_client.post(
+            f"{router_prefix}/nearest_hawkercentre",
+            json = {
+                "latitude": 1.3,
+                "longitude": 103.9,
+                "num_hawkercentres": 5
+            }
+        )
+
+        assert response.status_code == 200
+
+    def test_wrong_input(self, auth_client):
+        response = auth_client.post(
+            f"{router_prefix}/nearest_hawkercentre",
+            json = {
+                "test": 0
+            }
+        )
+
+        assert response.status_code == 422
+
+    def test_wrong_endpoint(self, auth_client):
+        response = auth_client.post(
+            f"/test",
+            json = {
+                "test": 0
+            }
+        )
+
+        assert response.status_code == 404
